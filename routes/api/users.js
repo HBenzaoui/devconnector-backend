55086fbc228e90a3a554bedb3c68463c56f940a1
Hamza Benzaoui <hamzapdd@gmail.com>
2019-01-30 05:47:56 +0100

    add the login REST API with cheking on email and password

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,7 @@ const User = require('../../models/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Users Works' }));
 
-// @route   GET api/users/register
+// @route   POST api/users/register
 // @des     Register new user
 // @access  Public
 router.post('/register', (req, res) => {
@@ -48,5 +48,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   POST api/users/login
+// @des     Login User / Returning JWT(JSON WEB Token)
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({ email }).then(user => {
+    // check for usr
+    if (!user) {
+      return res.status(404).json({ email: "This email doesn't exist" });
+    }
+
+    //Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
-1;
