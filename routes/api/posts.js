565eedacd598e3408358c7eb9641df6383869949
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-03 20:46:56 +0100

    add route for commenting and remove comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -158,4 +158,72 @@ router.post(
   }
 );
 
+// @route   POST api/posts/comment/:id
+// @des     Add comment to a post
+// @access  Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //Check validation
+    if (!isValid) {
+      //if any errors send 400 error
+      return res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        //Add to comments array
+        post.comments.unshift(newComment);
+
+        //save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @des     Remove comment from post
+// @access  Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        //Check if the comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexist: "Comment doesn't exists" });
+        }
+
+        //get remove index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //splice out of the array
+
+        post.comments.splice(removeIndex, 1);
+
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
--- validation/post.js
+++ validation/post.js
@@ -11,7 +11,7 @@ module.exports = function validatePostInput(data) {
   }
 
   if (Validator.isEmpty(data.text)) {
-    errors.text = 'Post text is required';
+    errors.text = 'Text is required';
   }
 
   return {
