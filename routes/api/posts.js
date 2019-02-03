f240c99836b95495f328430b55f13f583506ae56
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-03 19:10:50 +0100

    create route to get all posts and single post by the id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -15,7 +15,28 @@ const validatePostInput = require('../../validation/post');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
 
-// @route   POST api/posts/post
+// @route   GET api/posts
+// @des     Get all posts
+// @access  Public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopost: 'There are no posts' }));
+});
+
+// @route   GET api/posts/:id
+// @des     Get post by it ID
+// @access  Public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopost: 'This post is not available' })
+    );
+});
+
+// @route   POST api/posts
 // @des     Add new post
 // @access  Private
 router.post(
