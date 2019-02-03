463bc9454f4f851d9735ccbdb834b1e664e7e8a0
Hamza Benzaoui <hamzapdd@gmail.com>
2019-02-03 19:43:50 +0100

    add route to delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 //Load model
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 //Load Validation
 const validatePostInput = require('../../validation/post');
@@ -59,6 +60,34 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @des     Add new post
+// @des     Remove post
+// @access  Private
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          //Check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: 'User not authorized' });
+          }
+
+          //Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err =>
+          res.status(404).json({ nopost: 'This post is not available' })
+        );
+    });
   }
 );
 
